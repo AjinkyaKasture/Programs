1ed11e785a97155328090ed629b23a2b9f8521c8
AjinkyaKasture <40917961+AjinkyaKasture@users.noreply.github.com>
2018-07-11 21:14:50 +0530

    Added Third Program

---
+++ StrRev.java
@@ -0,0 +1,27 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class StrRev {
+	
+	public static void main(String[] args) {
+
+			String str ;
+			String rev = "";
+			
+			Scanner input = new Scanner(System.in); // It is Object of Scanner Class
+			
+			System.out.println("Enter the string : " );
+			str = input.next();
+
+			int strLength = str.length(); // Varible strLength stores the Length of String
+
+			for(int i = strLength-1 ; i >= 0; i--) {
+				
+				rev = rev + str.charAt(i); //Variable rev stores the Reverse of a String
+			}
+
+
+			System.out.println("The Reverse String is : " +rev ); // Printing The Reverse String
+			
+	}
+}
